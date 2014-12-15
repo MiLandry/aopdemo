795fc740c21811669dda565d0f5842fe84cbedf3
Michael <mlandryviolin@gmail.com>
2014-12-15 14:35:55 -0500

    now working

--- aopspringdemo/src/main/java/aopspringdemo/Main.java
+++ aopspringdemo/src/main/java/aopspringdemo/Main.java
@@ -1,11 +1,22 @@
 package aopspringdemo;
 
+import model.Triangle;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main
 {
 
     public static void main(String[] args)
     {
         System.out.println("hello, aop");
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+        Triangle triangle = (Triangle) ctx.getBean("triangle");
+        
+        triangle.draw();
+
+
         
 
     }
