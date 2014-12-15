c161a16a5cea58ba960da1a9af505b532fdc67ad
Michael <mlandryviolin@gmail.com>
2014-12-15 14:41:04 -0500

    aoeiaoei

---
+++ aopspringdemo/src/main/java/model/Triangle.java
@@ -0,0 +1,24 @@
+package model;
+
+public class Triangle
+{
+    private int size;
+
+    public int getSize()
+    {
+        return size;
+    }
+
+    public void setSize(int size)
+    {
+        this.size = size;
+    }
+
+    public void draw()
+    {
+        System.out.println("triangle drawn");
+    }
+    
+    
+
+}
