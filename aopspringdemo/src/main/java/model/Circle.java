71821f9d7b3e20f84460a494dc02f49da40b89ce
mlandry <mlandry@.orgstrategies.com>
2014-12-16 21:04:52 -0500

    added a circle!!!

---
+++ aopspringdemo/src/main/java/model/Circle.java
@@ -0,0 +1,24 @@
+package model;
+
+public class Circle
+{
+    private int size;
+
+    public int getSize()
+    {
+        return size;
+    }
+
+    public void setSize(int size)
+    {
+        this.size = size;
+    }
+
+    public void draw()
+    {
+        System.out.println("Circle drawn");
+    }
+    
+    
+
+}
